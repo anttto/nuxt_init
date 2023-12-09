fd11e42d60d46b9a1fd1a0902d59c19fc1624e87
anttto <dosaeun@gmail.com>
2023-12-10 01:01:02 +0900

    Computed를 이용한 Props, Emit 사용 예시 추가

---
+++ components/Child.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="background-color: #eee; padding: 1rem">
+    <h2>아이템 추가하기</h2>
+    <ul>
+      <li v-for="(obj, idx) in computedItem" :key="idx">{{ obj }}</li>
+    </ul>
+    <form action="" @submit.prevent="handleAddItem">
+      <input v-model="newItemName" type="text" />
+      <button type="submit">부모에게 데이터 올리기 (Emit)</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, defineProps, defineEmits } from 'vue';
+const newItemName = ref('');
+const props = defineProps({
+  propData: {
+    type: Object,
+    required: true,
+  },
+});
+const { propData } = props;
+const emit = defineEmits(['newItem']);
+
+// 부모에게 받은 Props Data를 특정한 상황에 따라 가공하여 부모로 올려야 하는 경우
+const computedItem = computed({
+  // 1.Props로 내려받은 데이터 컴포넌트에 반환하는 경우 -> 일반적인 get처리
+  get() {
+    return propData.items;
+  },
+  // 2.(읽기전용)인 computed를 가공하여 다시 반환하는 경우
+  set(newItem) {
+    emit('newItem', newItem);
+  },
+});
+
+// Input State
+const handleAddItem = () => {
+  computedItem.value = newItemName.value;
+  newItemName.value = '';
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/Parents.vue
@@ -0,0 +1,42 @@
+<template>
+  <div style="background-color: #f90; padding: 1rem">
+    <h3>
+      나의 보유 아이템: {{ data.items }}
+      <em class="num">
+        {{ data.items.length }}
+      </em>
+    </h3>
+    <Child :prop-data="data" @new-item="handleAddItem" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue';
+
+const data = reactive({
+  name: '도사은',
+  items: ['양손검', '십자방패'],
+});
+
+const handleAddItem = (newHobby: string) => {
+  console.log('Emit Data :', newHobby);
+  data.items = [...data.items, newHobby];
+};
+</script>
+
+<style lang="scss" scoped>
+.num {
+  font-weight: bold;
+  background-color: black;
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 100%;
+  font-size: 1.2rem;
+  font-style: normal;
+  color: #fff;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
