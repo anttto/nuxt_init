480ef32e68ff3e444835cf5fa2a638cc71c7dba2
anttto <dosaeun@gmail.com>
2024-02-09 16:58:59 +0900

    NuxtImg Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [['@nuxtjs/eslint-module', {}]],
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
